f17753d1799c971145e2977d0bd5de165681b62b
huazhimeng <2902151499@qq.com>
2025-02-13 10:44:57 +0800

    提交

--- sid.go
+++ sid.go
@@ -7,7 +7,7 @@ import (
 )
 
 // FromStr 从字符串创建id
-func FromStr(str string) (id Id) {
+/*func FromStr(str string) (id Id) {
 	buf, err := decode(str)
 	if err != nil {
 		log.Println(err)
@@ -24,15 +24,15 @@ func FromStr(str string) (id Id) {
 
 	id = Id(res)
 	return
-}
+}*/
 
 // FromNum 从数字创建id
-func FromNum(num uint64) (id Id) {
+/*func FromNum(num uint64) (id Id) {
 	return Id(num)
-}
+}*/
 
 // New 获取一个id
-func New() (id Id) {
+/*func New() (id Id) {
 	nextID, err := sf.NextID()
 	if err != nil {
 		log.Println(err)
@@ -40,4 +40,35 @@ func New() (id Id) {
 	}
 	id = Id(nextID)
 	return
+}*/
+
+func Generate() int64 {
+	nextID, err := sf.NextID()
+	if err != nil {
+		log.Println(err)
+		return 0
+	}
+	return int64(nextID)
+}
+
+func Encode(num int64) string {
+	return Id(uint64(num)).String()
+}
+
+func Decode(encoded string) (result int64) {
+	buf, err := decode(encoded)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if len(buf) < 8 {
+		err = fmt.Errorf("buf len < 8")
+		log.Println(err)
+		return
+	}
+
+	res := binary.LittleEndian.Uint64(buf)
+
+	return int64(Id(res).Uint64())
 }
